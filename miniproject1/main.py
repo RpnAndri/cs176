24cc4c9660af4dbe3960e8842640020721a3d36d
RpnAndri <andri.crisostomo@gmail.com>
2025-09-06 18:24:01 +0800

    add: miniproject 1

---
+++ miniproject1/main.py
@@ -0,0 +1,21 @@
+# Group 2
+# Crisostomo, Alessandro
+# Ramos, Gab
+# Senatin, Christopher
+
+import tkinter as tk
+import cv2
+
+# Cyndaquill colors
+# Main Color 
+CYND_BODY = "#306B84"
+CYND_BELLY = "#FFF7A5"
+
+# Tkinter
+root = tk.Tk()
+root.title("Group 2 - Mini Project")
+root.geometry("500x500")
+root.configure(bg=CYND_BODY)
+
+root.mainloop()
+
