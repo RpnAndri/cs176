f3053614ff895803e8422e5866b0e10752494c27
RpnAndri <andri.crisostomo@gmail.com>
2025-09-06 21:34:31 +0800

    mediapipe implementation

--- miniproject1/main.py
+++ miniproject1/main.py
@@ -7,6 +7,7 @@ import tkinter as tk
 from tkmacosx import Button
 from PIL import Image, ImageTk
 import cv2
+import mediapipe as mp
 
 # Global Variables
 cap = None
@@ -15,12 +16,27 @@ running = False
 WIDTH = 500
 HEIGHT = 300
 
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
+mp_hands = mp.solutions.hands
+hands = mp_hands.Hands(
+    model_complexity=0,
+    min_detection_confidence=0.5,
+    min_tracking_confidence=0.5
+)
+
+
 # Cyndaquill colors
 # Main Color 
 CYND_BODY = "#306B84"
 CYND_BELLY = "#FFF7A5"
 CYND_RED = "#FF3701"
 
+# Count the number of fingers given the landmarks
+def count_fingers(hand_landmarks) -> int:
+    print(hand_landmarks)
+    print("\n\n\n\n")
+    return 1
 
 
 # OpenCV
@@ -40,6 +56,34 @@ def update():
             frame = cv2.resize(frame, (WIDTH, HEIGHT))
 
             frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+            results = hands.process(frame)
+
+            if results.multi_hand_landmarks:
+                for hand_landmarks in results.multi_hand_landmarks:
+                    # Draw the detected fingers on the frame
+                    mp_drawing.draw_landmarks(
+                        frame, 
+                        hand_landmarks, 
+                        mp_hands.HAND_CONNECTIONS,
+                        mp_drawing_styles.get_default_hand_landmarks_style(),
+                        mp_drawing_styles.get_default_hand_connections_style(),
+                    )
+
+                    fingers = count_fingers(hand_landmarks)
+
+                    match fingers:
+                        case 1:
+                            print(1)
+                        case 2:
+                            print(2)
+                        case 3:
+                            print(3)
+                        case 4:
+                            print(4)
+                        case 5:
+                            print(5)
+
             img = Image.fromarray(frame)
             imgtk = ImageTk.PhotoImage(image=img)
             video_label.imgtk = imgtk
