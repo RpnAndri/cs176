4767a1f6435846f874b3de40fd5059fa7c94b300
RpnAndri <andri.crisostomo@gmail.com>
2025-09-06 19:14:58 +0800

    add: button

--- miniproject1/main.py
+++ miniproject1/main.py
@@ -4,6 +4,7 @@
 # Senatin, Christopher
 
 import tkinter as tk
+from tkmacosx import Button
 import cv2
 
 # Cyndaquill colors
@@ -11,11 +12,27 @@ import cv2
 CYND_BODY = "#306B84"
 CYND_BELLY = "#FFF7A5"
 
+# OpenCV
+def foo():
+    print("Hello World")
+
+
 # Tkinter
 root = tk.Tk()
 root.title("Group 2 - Mini Project")
-root.geometry("500x500")
+root.geometry("640x520")
 root.configure(bg=CYND_BODY)
+
+btn = Button(
+    root,
+    text="Start",
+    command=foo,
+    bg=CYND_BELLY,
+    padx=20,
+    pady=10,
+    borderless=True,
+)
+btn.pack(expand=True)
 
 root.mainloop()
 
